a5e1f39f8aeff7a3359edc002a61beb197dd79e3
sauravkh <skharb@uw.edu>
2017-04-17 19:17:31 -0700

    final commit for first 300,000

--- twitter.R
+++ twitter.R
@@ -7,49 +7,28 @@ library("httr")
 library("xlsx")
 
 # set up OAuth
-consumer_key <- "aoQ1bAoMZ74pY5RsYcbKJzBUc"
-consumer_secret <- "spidig71mvpnQbGOWOuBvQC8vq9wP8ax4eeV9FzjlypeHjenM9"
-access_token <- "835019765508788224-WpZ2IPJgu1PKcA6w7rP628DPFystHsD"
-access_secret <- "0JDPxoIQvcukZ8rqoFIDDssrxRkglWvCwdG2dtWT7D1Fu"
+consumer_key <- "JjqU6C81ZSFttww8Xe2lWXrCg"
+consumer_secret <- "7MQDARYlC3f7sX5TlstvEHI3BYVjTOiFvRtoiu8fpb5NjBSwny"
+access_token <- "835019765508788224-Twx0VMQM48xsdDV0OQ4Q1ZjGdmGrOTt"
+access_secret <- "N950rLZolhw3I1RLhzujjnqZXTRdu8yueS5HVoL62XLHh"
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-oreo <- read.table("data/oreo.txt") #set appropriate path
+oreo <- read.table("data/oreo.txt", stringsAsFactors = FALSE)
+a <- unique(oreo) #set appropriate path
 
+#######################################
+#### Code for one id per api call #####
+#######################################
 
-i = 1
-for(i in 1:5) { # change number to the total number of id's in the file
-  # uncomment to change api calls to 10 at a time
-  # ids <- oreo[i,1]
-  # count = 2
-  # while(count < 11){
-  #   ids <- c(ids, oreo[count,1]) 
-  #   count <- count + 1
-  # }
-  # print(ids)
-  tryCatch({
-    # call api for 10 ids at a time 
-    tweet <- showStatus(oreo[i,1])
-    data <- as.data.frame(tweet)
-    print(data)
-    write.xlsx(data, "data/working.xlsx", append = TRUE)
-    #write.table(data, "data/oreoData.txt", append = TRUE)
-   # write.xlsx(data, "data/oreo.xlsx")
-  })
-  Sys.sleep(2)
-  #i <- i + 1 # cahnge to 10 if making 10 calls at a time
-}
-
-a <- unique(oreo)
-# single tweet at a time code here :
-
-for(i in 42958:60000) {
-  Sys.sleep(1)
+for(i in 280000:300000) {
+  Sys.sleep(0.5)
   tryCatch({
     tweet <- showStatus(a[i,1])
     data <- as.data.frame(tweet)
+    print(paste0("*********  ", i , "  **********"))
     print(data)
-    name <- paste0("data/tweet_", i, ".xlsx")
+    name <- paste0("data/new/tweet_", i, ".xlsx")
     write.xlsx(data, name)
     rm(tweet,data)
   }, error=function(e){
@@ -57,26 +36,48 @@ for(i in 42958:60000) {
   })
 }
 
-
 # Code for checking 
-tweet <- showStatus(oreo[4,1])
+tweet <- showStatus(oreo[1,1])
 data <- as.data.frame(tweet)
 print(data)
 
-
 # combine multiple data files into one 
-require(xlsx)
-
-DF <- read.xlsx("data/tweet_1.xlsx",1)
-
+require("xlsx")
+DF <- read.xlsx("data/tweet_10000.xlsx",1)
 temp = list.files(path="data/",pattern="*.xlsx")
-
+first_half <- read.xlsx("data/Merged/first_12075_oreo.xlsx",1)
+final <- rbind(first_half,DF)
+write.csv(final, "data/merged/89215TWEETS.csv")
 
 for(i in temp) {
   currentDF <- read.xlsx(paste0("data/", i),1)
   DF <- rbind(DF, currentDF)
 }
 
+#######################################
+#### Code for 10 id's per api call ####
+#######################################
 
-write.xlsx(DF, "data/first_12075_oreo.xlsx")
+big.data <- showStatus(a[1,1])
+big.data <- as.data.frame(big.data)
+start.from = 1
+while(start.from < 41){
+  # for every 10 from selected id's
+  for(repeat.calls in 1:10){ 
+    list.of.ids <- a[start.from,1] # first id
+    # to get 10 id's
+    for(id.number in 1:9){
+      list.of.ids <- c(list.of.ids, a[start.from + id.number,1]) # rest 8
+    }
+    # call api
+    raw.api.data <- lookup_statuses(list.of.ids)
+    current.dataframe <- as.data.frame(raw.api.data[[1]])
+    for(i in 2:length(raw.api.data)){
+      current.dataframe <- rbind(current.dataframe, as.data.frame(raw.api.data[[i]]))
+    } 
+  }
+  
+  big.data <- rbind(big.data, current.dataframe)
+  start.from = start.from + 10;
+}
 
--- twitter2.R
+++ twitter2.R
@@ -1,25 +1,81 @@
+# Saurav Kharb
+# Research Assitant
+# This script makes calls on the twitteR api using tweet id's
+
 library(twitteR)
+library(httr)
 library(xlsx)
-consumer_key <- "JjqU6C81ZSFttww8Xe2lWXrCg"
-consumer_secret <- "7MQDARYlC3f7sX5TlstvEHI3BYVjTOiFvRtoiu8fpb5NjBSwny"
-access_token <- "	835019765508788224-Twx0VMQM48xsdDV0OQ4Q1ZjGdmGrOTt"
-access_secret <- "N950rLZolhw3I1RLhzujjnqZXTRdu8yueS5HVoL62XLHh"
+consumer_key <- "QHJgBeTm2KW5yyKu5jwzFxioT"
+consumer_secret <- "TEvaRBKUaTM41lFzsiiclO7bIUHBZrarPsRPdL647ACMPO6uZe"
+access_token <- "835019765508788224-HUGrNbQLek454Mwf6NBLJNRIxPYwFsI"
+access_secret <- "xNzEIhcuO1WaV5hKGQItIhtCHLLbmAMHg5Y9Jy8S4zJfk"
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-oreo <- read.table("data/oreo.txt")
+oreo <- read.table("data/oreo.txt", stringsAsFactors = FALSE)
 a <- unique(oreo)
 
-for(i in 40364:80000) {
-  Sys.sleep(2)
+#######################################
+#### Code for one id per api call #####
+#######################################
+
+for(i in 640000:590000) {
+  Sys.sleep(0.7)
   tryCatch({
-    tweet <- showStatus(a[5,1])
+    tweet <- showStatus(a[2,1])
     data <- as.data.frame(tweet)
+    print(paste0("*********  ", i , "  **********"))
     print(data)
-    name <- paste0("data/tweet_", i, ".xlsx")
+    name <- paste0("data/new/tweet_", i, ".xlsx")
     write.xlsx(data, name)
     rm(tweet,data)
   }, error=function(e){
     print("I AM DUMB AND I AM BEHAVING WEIRDLY")
   })
 }
+
+
+write.csv(second.half,"data/merged/lastpeices.csv")
+first.half <- read.csv("data/merged/89215TWEETS.csv")
+second.half <- read.csv("data/merged/lastHalf.csv")
+boss <- rbind(boss,second.half)
+
+write.csv(boss,"FINAL_297679_TWEETS.csv")
+
+
+write.xlsx(boss,"data/merged/FINAL_XLSX_VERSION.xlsx")
+
+newData <- boss[65534,]
+
+for(i in 65535:297679){
+  newData <- rbind(newData,boss[i,])
+  print(i)
+}
+
+#######################################
+#### Code for 10 id's per call ########
+#######################################
+
+big.data <- showStatus(a[1,1])
+big.data <- as.data.frame(big.data)
+start.from = 1
+while(start.from < 41) {
+  # for every 10 from selected id's
+  for(repeat.calls in 1:10){ 
+    list.of.ids <- a[start.from,1] # first id
+    # to get 10 id's
+    for(id.number in 1:9){
+      list.of.ids <- c(list.of.ids, a[start.from + id.number,1]) # rest 8
+    }
+    # call api
+    raw.api.data <- lookup_statuses(list.of.ids)
+    current.dataframe <- as.data.frame(raw.api.data[[1]])
+    for(i in 2:length(raw.api.data)){
+      current.dataframe <- rbind(current.dataframe, as.data.frame(raw.api.data[[i]]))
+    } 
+  }
+  
+  big.data <- rbind(big.data, current.dataframe)
+  start.from = start.from + 10;
+}
+
